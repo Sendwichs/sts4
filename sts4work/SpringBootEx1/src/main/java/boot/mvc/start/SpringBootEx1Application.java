bbb9598ea46019d1ffeeb7493026221f5d149cf4
sung <tjdgksrud123@naver.com>
2023-04-12 11:52:09 +0900

    11:52

--- sts4work/SpringBootEx1/src/main/java/boot/mvc/start/SpringBootEx1Application.java
+++ sts4work/SpringBootEx1/src/main/java/boot/mvc/start/SpringBootEx1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "boot.test", "boot.hello" }) // {} -> 복수 선택
 public class SpringBootEx1Application {
 
 	public static void main(String[] args) {
---
+++ sts4work/SpringBootEx1/src/main/java/boot/test/TestController.java
@@ -0,0 +1,18 @@
+package boot.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public TestDto hello() {
+		TestDto dto = new TestDto();
+
+		dto.setName("성한경");
+		dto.setAddr("서울시 강남구");
+
+		return dto;
+	}
+}

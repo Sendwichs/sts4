febebea8f37e58ec0e4ed5222a6961a735e45555
sung <tjdgksrud123@naver.com>
2023-04-12 12:03:52 +0900

    12:03

---
+++ sts4work/BootWebEx2/src/main/java/boot/mvc/coffee/BootWebEx2Application.java
@@ -0,0 +1,13 @@
+package boot.mvc.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebEx2Application.class, args);
+	}
+
+}
